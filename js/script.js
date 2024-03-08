e04c913e2557b97d520dd44dba412ce9216707e2
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-08 15:27:20 +0100

    fix: js organization

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
-/* Gioco dei dadi */
+/* -----------------
+   GIOCO DEI DADI
+----------------- */
+/* 
 // Chiedo all'utente di scrivere un numero casuale da 1 a 6
-const userDice = parseInt(prompt('Inserisci un numero da 1 a 6'))
-console.log('dado utente', userDice)
 // Creo un numero casuale da 1 a 6 anche per il computer
-const comDice = Math.floor(Math.random() * 6) + 1;
-console.log('dado pc', comDice);
 // Stabilisco il vincitore
 // - Se il numero dell'utente è superiore a quello del pc scrivo 'Complimenti hai vinto'
 // - Altrimenti se i due numeri sono uguali scrivo 'Pareggio'
 // - Altrimenti scrivo 'Mi dispiace hai perso'
+// Stampo il risultato nel DOM 
+*/
+// Variabili
+const userDice = parseInt(prompt('Inserisci un numero da 1 a 6'))
+const comDice = Math.floor(Math.random() * 6) + 1;
+const userMessage = document.querySelector('#message');
 let winnerOrLose;
+// Funzionalità
 if (userDice > comDice) {
     winnerOrLose = 'Complimenti hai vinto!!!';
 } else if (userDice === comDice) {
@@ -17,7 +23,4 @@ if (userDice > comDice) {
 } else {
     winnerOrLose = 'Mi dispiace hai perso';
 }
-console.log(winnerOrLose);
-// Stampo il risultato nel DOM
-const userMessage = document.querySelector('#message');
 userMessage.innerHTML = winnerOrLose;
